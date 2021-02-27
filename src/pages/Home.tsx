05775d4ba3ffe6dd478d276d83ccb1fc07c49167
al.co.js <al.co.js@kakao.com>
2021-02-27 21:28:56 +0900

    feat: home container

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import LargeButton from '../components/LargeButton';
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  max-width: 360px;
+  justify-content: center;
+`;
 
 const Home: React.FC = () => (
   <>
-    {
-      // STUB Generated default code
-    }
+    <Wrapper>
+      <Container>
+        <LargeButton>시작하기</LargeButton>
+      </Container>
+    </Wrapper>
   </>
 );
 
