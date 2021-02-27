52bca28ff2ad4e2f30ae1a3b232c092944d03e5f
al.co.js <al.co.js@kakao.com>
2021-02-27 21:23:59 +0900

    feat: NEXONFootballGothic font

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,33 @@ import styled, { createGlobalStyle } from 'styled-components';
 import Router from './Router';
 
 const GlobalStyle = createGlobalStyle`
-  // TODO Check the font list with the designer and add the font face in four forms: TTF, OTF, WOFF, and WOFF2
+  // TODO Check the font list with the designer and add the font face in five forms: TTF, EOT, SVG, WOFF, and WOFF2
+
+  @font-face {
+    font-family: 'NEXON Football Gothic L';
+    src: url('/fonts/NEXONFootballGothicL.eot');
+    src: url('/fonts/NEXONFootballGothicL.eot?#iefix') format('embedded-opentype'),
+        url('/fonts/NEXONFootballGothicL.woff2') format('woff2'),
+        url('/fonts/NEXONFootballGothicL.woff') format('woff'),
+        url('/fonts/NEXONFootballGothicL.ttf') format('truetype'),
+        url('/fonts/NEXONFootballGothicL.svg#NEXONFootballGothicL') format('svg');
+    font-weight: 300;
+    font-style: normal;
+    font-display: block;
+  }
+
+  @font-face {
+      font-family: 'NEXON Football Gothic B';
+      src: url('/fonts/NEXONFootballGothicB.eot');
+      src: url('/fonts/NEXONFootballGothicB.eot?#iefix') format('embedded-opentype'),
+          url('/fonts/NEXONFootballGothicB.woff2') format('woff2'),
+          url('/fonts/NEXONFootballGothicB.woff') format('woff'),
+          url('/fonts/NEXONFootballGothicB.ttf') format('truetype'),
+          url('/fonts/NEXONFootballGothicB.svg#NEXONFootballGothicB') format('svg');
+      font-weight: bold;
+      font-style: normal;
+      font-display: block;
+  }
 
   body,
   html {
@@ -21,7 +47,6 @@ const GlobalStyle = createGlobalStyle`
 
 const Wrapper = styled(BrowserRouter)`
   flex: 1 0 auto;
-  padding-bottom: 20px;
 `;
 
 const App: React.FC = () => (
