424fe8f1fd03a003c15195117e40c7aecd88d643
al.co.js <al.co.js@kakao.com>
2021-02-27 21:25:52 +0900

    feat: large button

--- src/components/LargeButton.tsx
+++ src/components/LargeButton.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const LargeButton: React.FC = () => (
+import { LargeButtonProps } from '../types';
+
+const Button = styled.button<LargeButtonProps>`
+  max-width: 283px;
+  background-color: #70c67e;
+  border: none;
+  width: 80%;
+  height: 50px;
+  font-size: 1rem;
+  color: #ffffff;
+  border-radius: 5px;
+  font-family: 'NEXON Football Gothic B';
+`;
+
+const LargeButton: React.FC<LargeButtonProps> = (props: LargeButtonProps) => (
   <>
-    {
-      // STUB Generated default code
-    }
+    <Button type="button" {...props} />
   </>
 );
 
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export type LargeButtonProps = {
+  children?: React.ReactNode;
+  onClick?: () => void;
+};
