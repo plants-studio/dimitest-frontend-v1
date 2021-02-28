ef63a19efe756a0249b12719deca413e9393d7ac
al-co-js <al.co.js@kakao.com>
2021-02-28 19:38:09 +0900

    feat: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Header: React.FC = () => (
-  <>
-    {
-      // STUB Generated default code
-    }
-  </>
-);
+import { HeaderProps } from '../types';
+
+const Text = styled.div<HeaderProps>`
+  font-family: ${(props: HeaderProps) => (props.size === 'Large' ? 'NEXON Football Gothic B' : 'NanumSquare')};
+  font-size: ${(props: HeaderProps) => (props.size === 'Large' && '1.6875rem')
+    || (props.size === 'Medium' && '1rem')
+    || (props.size === 'Small' && '0.625rem')};
+  font-weight: ${(props: HeaderProps) => (props.size === 'Small' ? 'bold' : 'normal')};
+  color: ${(props: HeaderProps) => props.size !== 'Large' && '#CBCBCB'};
+`;
+
+const Header: React.FC<HeaderProps> = (props: HeaderProps) => <Text {...props} />;
 
 export default Header;
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,8 @@ export type LargeButtonProps = {
   children?: React.ReactNode;
   onClick?: () => void;
 };
+
+export type HeaderProps = {
+  size: 'Large' | 'Medium' | 'Small';
+  children?: React.ReactNode;
+}
